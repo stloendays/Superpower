ae5d6d753d4d9416a55aa53cb43594e4af8180fe
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-15 22:17:51 +0800

    feat: expose workflow binding and runner controls in review dialog

--- desktop/qt/src/workflowreviewdialog.h
+++ desktop/qt/src/workflowreviewdialog.h
@@ -3,6 +3,7 @@
 #include <QDialog>
 #include <QJsonArray>
 #include <QJsonObject>
+#include <QStringList>
 
 class QLabel;
 class QListWidget;
@@ -15,20 +16,40 @@ class WorkflowReviewDialog final : public QDialog {
  public:
   explicit WorkflowReviewDialog(QWidget *parent = nullptr);
   void setPlan(const QJsonObject &plan);
+  void setRunState(const QJsonObject &state);
+  void setRunError(const QString &message);
 
  signals:
   void reviewActionRequested(const QJsonObject &actionPlan);
+  void startRunRequested(const QString &planId, const QStringList &approvedBindingIds);
+  void advanceRunRequested(bool approve);
+  void provideOutputRequested(const QString &stepId, const QString &output);
 
  private:
   void buildUi();
   void applyStyle();
   void updateSelectedStep();
+  void updateRunControls();
   void reviewSelectedAction();
+  void startReviewedRun();
+  void advanceReviewedRun();
+  void provideManualOutput();
+  QStringList approvedBindingIds() const;
 
   QJsonObject plan_;
+  QJsonObject runState_;
   QJsonArray steps_;
+  QJsonArray bindings_;
+  QString planId_;
+  bool advanceRequiresApproval_ = false;
+
   QLabel *summaryLabel_ = nullptr;
+  QLabel *runStatusLabel_ = nullptr;
   QListWidget *stepList_ = nullptr;
+  QListWidget *bindingList_ = nullptr;
   QPlainTextEdit *detailView_ = nullptr;
   QPushButton *reviewButton_ = nullptr;
+  QPushButton *startRunButton_ = nullptr;
+  QPushButton *advanceButton_ = nullptr;
+  QPushButton *manualOutputButton_ = nullptr;
 };
