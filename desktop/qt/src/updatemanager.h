e02e66e9dd99f0778edb03e4a938da7389795d76
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-16 12:01:06 +0800

    desktop: add GitHub release update manager interface

---
+++ desktop/qt/src/updatemanager.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <QNetworkAccessManager>
+#include <QObject>
+#include <QUrl>
+
+class UpdateManager final : public QObject {
+  Q_OBJECT
+
+public:
+  explicit UpdateManager(QObject *parent = nullptr);
+
+  void scheduleStartupCheck();
+  void checkForUpdates(bool userInitiated = false);
+  void installDownloadedUpdate();
+
+  bool updateReady() const;
+  QString availableVersion() const;
+
+signals:
+  void statusChanged(const QString &message);
+  void updateAvailable(const QString &version);
+  void updateReadyToInstall(const QString &version);
+  void alreadyUpToDate(const QString &version);
+  void updateError(const QString &message, bool userInitiated);
+
+private:
+  QString currentVersion() const;
+  bool isNewerVersion(const QString &candidate) const;
+  void fetchChecksumAndPackage();
+  void downloadPackage();
+  void fail(const QString &message);
+
+  QNetworkAccessManager network_;
+  QString availableVersion_;
+  QString assetName_;
+  QUrl packageUrl_;
+  QUrl checksumsUrl_;
+  QByteArray expectedSha256_;
+  QString downloadedArchive_;
+  bool checking_{false};
+  bool downloading_{false};
+  bool userInitiated_{false};
+};
