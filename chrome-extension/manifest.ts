1ff41c1960cdf4fcd85db5ff5a68f653f36f8917
Tony <157110190+stloendays@users.noreply.github.com>
2026-09-17 19:33:10 +0800

    chore: remove inherited Firefox extension identity

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -7,10 +7,6 @@ const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
  * if you want to support multiple languages, you can use the following reference
  * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Internationalization
  *
- * @prop browser_specific_settings
- * Must be unique to your extension to upload to addons.mozilla.org
- * (you can delete if you only want a chrome extension)
- *
  * @prop permissions
  * Firefox doesn't support sidePanel (It will be deleted in manifest parser)
  *
@@ -21,11 +17,6 @@ const manifest = {
   manifest_version: 3,
   default_locale: 'en',
   name: 'Superpower',
-  browser_specific_settings: {
-    gecko: {
-      id: 'saurabh@mcpsuperassistant.ai',
-    },
-  },
   version: packageJson.version,
   version_name: `V${packageJson.version}`,
   description: 'Run MCP tools from supported AI web apps.',
